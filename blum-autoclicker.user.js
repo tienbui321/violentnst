46db26acec2efbcfa758c63a019bcab0dc44ad68
tienbui321 <tienbva@gmail.com>
2024-09-14 22:15:19 +0700

    Add files via upload

---
+++ blum-autoclicker.user.js
@@ -0,0 +1,172 @@
+﻿// ==UserScript==
+// @name         Blum Autoclicker
+// @version      1.0
+// @namespace    Violentmonkey Scripts
+// @author       TienBV
+// @match        https://telegram.blum.codes/*
+// @grant        none
+// @icon
+// @downloadURL
+// @updateURL
+// @homepage     https://github.com/S3C237/Blum
+// ==/UserScript==
+
+let GAME_SETTINGS = {
+    minBombHits: 0, // Math.floor(Math.random() * 2), // Minimum number of bomb clicks in percentage
+    minIceHits: Math.floor(Math.random() * 2) + 2, // Minimum number of freeze clicks
+    flowerSkipPercentage: Math.floor(Math.random() * 11) + 15, // Probability of clicking on a flower in percentage
+    minDelayMs: 2000, // Minimum delay between actions in milliseconds
+    maxDelayMs: 5000, // Maximum delay between actions in milliseconds
+};
+
+let isGamePaused = false;
+
+try {
+    let gameStats = {
+        score: 0,
+        bombHits: 0,
+        iceHits: 0,
+        flowersSkipped: 0,
+        isGameOver: false,
+    };
+
+    const originalPush = Array.prototype.push;
+    Array.prototype.push = function(...items) {
+        if (!isGamePaused) {
+            items.forEach(item => handleGameElement(item));
+        }
+        return originalPush.apply(this, items);
+    };
+
+    function handleGameElement(element) {
+        if (!element || !element.item) return;
+
+        const { type } = element.item;
+        switch (type) {
+            case "CLOVER":
+                processFlower(element);
+                break;
+            case "BOMB":
+                processBomb(element);
+                break;
+            case "FREEZE":
+                processIce(element);
+                break;
+        }
+    }
+
+    function processFlower(element) {
+        const shouldSkip = Math.random() < (GAME_SETTINGS.flowerSkipPercentage / 100);
+        if (shouldSkip) {
+            gameStats.flowersSkipped++;
+        } else {
+            gameStats.score++;
+            clickElement(element);
+        }
+    }
+
+    function processBomb(element) {
+        if (gameStats.bombHits < GAME_SETTINGS.minBombHits) {
+            gameStats.score = 0;
+            clickElement(element);
+            gameStats.bombHits++;
+        }
+    }
+
+    function processIce(element) {
+        if (gameStats.iceHits < GAME_SETTINGS.minIceHits) {
+            clickElement(element);
+            gameStats.iceHits++;
+        }
+    }
+
+    function clickElement(element) {
+        element.onClick(element);
+        element.isExplosion = true;
+        element.addedAt = performance.now();
+    }
+
+    function checkGameCompletion() {
+        const rewardElement = document.querySelector('#app > div > div > div.content > div.reward');
+        if (rewardElement && !gameStats.isGameOver) {
+            gameStats.isGameOver = true;
+            resetGameStats();
+            resetGameSettings();
+        }
+    }
+
+    function resetGameStats() {
+        gameStats = {
+            score: 0,
+            bombHits: 0,
+            iceHits: 0,
+            flowersSkipped: 0,
+            isGameOver: false,
+        };
+    }
+
+    function resetGameSettings() {
+        GAME_SETTINGS = {
+            minBombHits: 0, // Minimum number of bomb clicks in percentage
+            minIceHits: 5, //Math.floor(Math.random() * 2) + 2, // Minimum number of freeze clicks
+            flowerSkipPercentage: Math.floor(Math.random() * 11) + 15, // Probability of clicking on a flower in percentage
+            minDelayMs: 2000, // Minimum delay between actions in milliseconds
+            maxDelayMs: 5000, // Maximum delay between actions in milliseconds
+        };
+    }
+
+    function getNewGameDelay() {
+        return Math.floor(Math.random() * (3000 - 1000 + 1) + 1000);
+    }
+
+    function checkAndClickPlayButton() {
+        const playButton = document.querySelector('button.kit-button.is-large.is-primary');
+        if (!isGamePaused && playButton && playButton.textContent.includes('Play')) {
+            setTimeout(() => {
+                playButton.click();
+                gameStats.isGameOver = false;
+            }, getNewGameDelay());
+        }
+    }
+
+    function continuousPlayButtonCheck() {
+        checkAndClickPlayButton();
+        setTimeout(continuousPlayButtonCheck, 1000);
+    }
+
+    const observer = new MutationObserver(mutations => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList') {
+                checkGameCompletion();
+            }
+        }
+    });
+
+    const appElement = document.querySelector('#app');
+    if (appElement) {
+        observer.observe(appElement, { childList: true, subtree: true });
+    }
+
+    continuousPlayButtonCheck();
+
+    const pauseButton = document.createElement('button');
+    pauseButton.textContent = 'Pause';
+    pauseButton.style.position = 'fixed';
+    pauseButton.style.bottom = '20px';
+    pauseButton.style.right = '20px';
+    pauseButton.style.zIndex = '9999';
+    pauseButton.style.padding = '4px 8px';
+    pauseButton.style.backgroundColor = '#5d5abd';
+    pauseButton.style.color = 'white';
+    pauseButton.style.border = 'none';
+    pauseButton.style.borderRadius = '10px';
+    pauseButton.style.cursor = 'pointer';
+    pauseButton.onclick = toggleGamePause;
+    document.body.appendChild(pauseButton);
+
+    function toggleGamePause() {
+        isGamePaused = !isGamePaused;
+        pauseButton.textContent = isGamePaused ? 'Resume' : 'Pause';
+    }
+
+} catch (e) {}
