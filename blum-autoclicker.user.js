6b22c088880d40406827ca8626eee24336d30815
tienbui321 <tienbva@gmail.com>
2024-09-15 15:48:29 +0700

    Add files via upload

--- blum-autoclicker.user.js
+++ blum-autoclicker.user.js
@@ -1,12 +1,11 @@
 ﻿// ==UserScript==
 // @name         Blum Autoclicker
-// @version      1.0
+// @version      1.1
 // @namespace    Violentmonkey Scripts
 // @author       TienBV
 // @match        https://telegram.blum.codes/*
 // @grant        none
-// @icon
-// @downloadURL  https://github.com/tienbui321/violentnst/blob/main/blum-autoclicker.user.js   
+// @icon         https://github.com/tienbui321/violentnst/blob/main/blum.png
 // @updateURL    https://github.com/tienbui321/violentnst/blob/main/blum-autoclicker.user.js
 // @homepage     https://github.com/tienbui321/violentnst/blob/main/blum.png
 // ==/UserScript==
@@ -169,4 +168,100 @@ try {
         pauseButton.textContent = isGamePaused ? 'Resume' : 'Pause';
     }
 
-} catch (e) {}
+    const taskButton = document.createElement('button');
+    taskButton.textContent = 'Do Task';
+    taskButton.style.position = 'fixed';
+    taskButton.style.bottom = '20px';
+    taskButton.style.right = '90px';
+    taskButton.style.zIndex = '9999';
+    taskButton.style.padding = '4px 8px';
+    taskButton.style.backgroundColor = '#5d5abd';
+    taskButton.style.color = 'white';
+    taskButton.style.border = 'none';
+    taskButton.style.borderRadius = '10px';
+    taskButton.style.cursor = 'pointer';
+    taskButton.onclick = blum_doTask;
+    document.body.appendChild(taskButton);
+
+    let playtime = 0;
+
+    function blum_doTask() {
+        playtime++;
+        if (playtime > 2)
+            return;
+
+        _$(".layout-tabs.tabs a[href='/tasks']").click();
+        _clickToTab();
+    }
+
+    async function _clickToTab(idx) {
+        await sleep(3000);
+        idx = idx || 2;
+        if (idx == 3)
+            idx++;
+
+        let tab = _$('.pages-tasks-sub-sections .kit-tabs .list > label.show-dot:nth-child(' + idx + ') > span');
+        if (!tab || tab.length == 0) {
+            // chay het thi vong lai claim
+            blum_doTask();
+            return;
+        }
+
+        tab.click();
+        await sleep(3000);
+
+        _doTasksInTab();
+
+        await sleep(2000);
+        _clickToTab(++idx);
+    }
+
+    async function _doTasksInTab() {
+        let taskList = _$('.tasks-list > div');
+        if (!taskList || taskList.length === 0)
+            return;
+
+        for (var i = 0; i < taskList.length; i++) {
+            let task = taskList[i];
+            await sleep(1500);
+            clickTask(task);
+        }
+    }
+
+    async function clickTask(task) {
+        let taskBtn = _$('button.pill-btn', task);
+        let text = taskBtn.innerText.trim();
+        let is_not_started = hasClass(taskBtn, "is-status-not-started");
+        let is_claim = hasClass(taskBtn, "is-status-ready-for-claim");
+
+        if (is_not_started && !hasIgnoreTask(text))
+            taskBtn.click();
+        else if (is_claim == true) {
+            taskBtn.click();
+        }
+    }
+
+    function hasIgnoreTask(text) {
+        let ignoreText = ["Boost Blum"];
+        if (ignoreText.includes(text))
+            return true;
+        return false;
+    }
+
+    function _$(selector, parent) {
+        let result = parent ? parent.querySelectorAll(selector) : document.querySelectorAll(selector);
+        if (result.length === 1)
+            return result[0];
+        return result;
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function hasClass(element, className) {
+        return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+    }
+} catch (e) {
+    console.log(e);
+}
