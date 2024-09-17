f8323fa83c948e675cb044aea7bc0fbccdf8dbd3
tienbui321 <tienbva@gmail.com>
2024-09-17 11:31:43 +0700

    Add files via upload

--- blum-autoclicker.user.js
+++ blum-autoclicker.user.js
@@ -1,13 +1,13 @@
 ﻿// ==UserScript==
 // @name         Blum Autoclicker
-// @version      1.1
+// @version      1.2
 // @namespace    Violentmonkey Scripts
 // @author       TienBV
 // @match        https://telegram.blum.codes/*
 // @grant        none
-// @icon         https://github.com/tienbui321/violentnst/blob/main/blum.png
-// @updateURL    https://github.com/tienbui321/violentnst/blob/main/blum-autoclicker.user.js
-// @homepage     https://github.com/tienbui321/violentnst/blob/main/blum.png
+// @icon         https://github.com/tienbui321/violentnst/raw/main/blum.png
+// @updateURL    https://github.com/tienbui321/violentnst/raw/main/blum-autoclicker.user.js
+// @homepage     https://github.com/tienbui321/violentnst/raw/main/blum.png
 // ==/UserScript==
 
 let GAME_SETTINGS = {
