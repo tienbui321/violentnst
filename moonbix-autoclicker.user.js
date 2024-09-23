a30a76b025e59efe9d0e9547b6a5e7f52afd08b6
tienbui321 <tienbva@gmail.com>
2024-09-23 20:52:00 +0700

    Add files via upload

--- moonbix-autoclicker.user.js
+++ moonbix-autoclicker.user.js
@@ -1,17 +1,33 @@
 ﻿// ==UserScript==
 // @name         Moonbix Autoclicker TienBV
-// @version      1.0
+// @version      1.1
 // @namespace    Violentmonkey Scripts
 // @author       TienBV
 // @match        https://www.binance.com/*
 // @grant        none
 // @icon         https://github.com/tienbui321/violentnst/raw/main/blum.png
 // @updateURL    https://github.com/tienbui321/violentnst/raw/main/moonbix-autoclicker.user.js
-// @downloadURL  https://github.com/tienbui321/violentnst/blob/main/moonbix-autoclicker.user.js
+// @downloadURL  https://github.com/tienbui321/violentnst/raw/main/moonbix-autoclicker.user.js
 // @homepage     https://github.com/tienbui321/violentnst/raw/main/blum.png
 // ==/UserScript==
 
 try {
+
+    const taskButton = document.createElement('button');
+    taskButton.textContent = 'Do Task';
+    taskButton.id = 'taskAutoButton';
+    taskButton.style.position = 'fixed';
+    taskButton.style.bottom = '20px';
+    taskButton.style.right = '20px';
+    taskButton.style.zIndex = '9999';
+    taskButton.style.padding = '4px 8px';
+    taskButton.style.backgroundColor = '#5d5abd';
+    taskButton.style.color = 'white';
+    taskButton.style.border = 'none';
+    taskButton.style.borderRadius = '10px';
+    taskButton.style.cursor = 'pointer';
+    taskButton.onclick = _doAuto;
+    document.body.appendChild(taskButton);
 
     function simulateClick(element) {
         const event = new MouseEvent('mousedown', {
@@ -55,6 +71,8 @@ try {
                 playButton.click();
                 playGame();
             }, getNewGameDelay());
+        } else {
+            setTaskDone();
         }
     }
 
@@ -63,77 +81,36 @@ try {
         setTimeout(continuousPlayButtonCheck, 1000);
     }
 
-    continuousPlayButtonCheck();
+    function checkGameOver() {
+        let continueEl = _$(".Game_game__container__1I7MG > div > div:last-child button");
+        if (continueEl.textContent == "Continue") {
+            setTaskDone();
+            continueEl.click();
+        }
+    }
 
-    function clickTaskTab() {
-        const taskTab = Array.from(document.querySelectorAll('.components_container__tab__1mbN9')).find(tab => {
-            return tab.innerText.includes('Nhiệm vụ');
+    function setTaskDone() {
+        taskButton.textContent = "Done";
+        taskButton.classList.add("task_done");
+    }
+
+    async function regObserve() {
+        const observer = new MutationObserver(mutations => {
+            for (const mutation of mutations) {
+                if (mutation.type === 'childList') {
+                    checkGameOver();
+                }
+            }
         });
 
-        if (taskTab) {
-            console.log('Tab Nhiệm vụ được tìm thấy, đang click...');
-            taskTab.click();
-            return true;
-        } else {
-            console.log('Không tìm thấy tab Nhiệm vụ.');
-            return false;
+        const appElement = await waitAndSelect('#app');
+        if (appElement) {
+            observer.observe(appElement, { childList: true, subtree: true });
         }
+
     }
 
-    function clickIncompleteTask() {
-        const tasks = document.querySelectorAll('.Tasks_taskItem__16PwK');
-        let foundIncompleteTask = false;
-        for (const task of tasks) {
-            const checkIcon = task.querySelector('img[src*="check.png"]');
-            if (!checkIcon) {
-                console.log('Nhiệm vụ chưa hoàn thành được tìm thấy, đang click...');
-                task.click(); // Thực hiện click vào nhiệm vụ chưa hoàn thành
-                foundIncompleteTask = true;
-                break;
-            }
-        }
-        return foundIncompleteTask;
-    }
-
-    function clickContinueButton() {
-        const continueButton = document.querySelector('.DailyLogin_login__button__15aOK');
-        if (continueButton) {
-            console.log('Nút "Tiếp tục" được tìm thấy, đang click...');
-            continueButton.click(); // Thực hiện click vào nút "Tiếp tục"
-            return true;
-        } else {
-            console.log('Không tìm thấy nút "Tiếp tục".');
-            return false;
-        }
-    }
-
-    window.addEventListener('load', function() {
-        if (document.readyState === 'complete') {
-            // Thực hiện hàm click
-            let clicked = false;
-            const interval = setInterval(() => {
-                clicked = clickTaskTab();
-                if (clicked) {
-                    clearInterval(interval);
-                    const taskInterval = setInterval(() => {
-                        const found = clickIncompleteTask();
-                        if (!found) {
-                            clearInterval(taskInterval);
-                            console.log('Không tìm thấy nhiệm vụ chưa hoàn thành nào nữa.');
-                        }
-                    }, 1000);
-                }
-            }, 1000);
-
-            const continueInterval = setInterval(() => {
-                const clickedContinue = clickContinueButton();
-                if (clickedContinue) {
-                    clearInterval(continueInterval);
-                }
-            }, 1000);
-        }
-    });
-
+    continuousPlayButtonCheck();
 } catch (e) {
     console.log(e);
 }
